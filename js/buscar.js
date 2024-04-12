68c1637e34957ac6c745241bfabcdb19faa5c8ab
GustavoHMarchi <gustavo.holiveira@sptech.school>
2024-04-11 21:25:13 -0300

    create buscar.js file

---
+++ js/buscar.js
@@ -0,0 +1,13 @@
+async function buscar(){
+    console.log("Passei por aqui")
+    const resposta = await fetch("https://660f44af356b87a55c510d73.mockapi.io/agendas")
+    const respostaDadosAgenda = await resposta.json()
+    console.log("Resposta:", respostaDadosAgenda)
+    const cards = document.getElementById("cards_games")
+    cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
+        return "TESTE" 
+    })
+}
+console.log("antes de buscar")
+buscar()
+console.log("depois de buscar")
